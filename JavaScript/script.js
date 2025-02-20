d5ed85ddeae8cccd211f1f72b6b61cf51dba476b
Rajesh Kumar <rajesh.inboundwebhub@gmail.com>
2025-02-20 17:46:29 +0530

    20 Feb 2025

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+
+// This code of externalScript.html
+function orangeColor(){
+    document.getElementById('fun').style.color='white';
+    document.getElementById('divfun').style.backgroundColor='black';
+  }
+
+  function redColor(){
+    document.getElementById('fun').style.color='red';
+    document.getElementById('divfun').style.backgroundColor='blue';
+  }
+
+  function greenColor(){
+    document.getElementById('fun').style.color='green';
+    document.getElementById('divfun').style.backgroundColor='pink';
+  }
+
+  function myFunction(){
+    document.getElementById('fun').style.color='black';
+    document.getElementById('divfun').style.backgroundColor='white';
+  }
+
