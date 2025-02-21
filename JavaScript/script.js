8aa0438f2b9c466e876cdc1ded21d42fd02a04d3
Rajesh Kumar <rajesh.inboundwebhub@gmail.com>
2025-02-21 19:47:51 +0530

    21 Feb 2025

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -20,3 +20,107 @@ function orangeColor(){
     document.getElementById('divfun').style.backgroundColor='white';
   }
 
+
+
+
+//this Code of srithmeticOperator.html
+  function addition(x,y){
+    let result = x + y;
+   document.getElementById('Addition').innerHTML = "Addition of 100+21 =" +result;
+}
+
+function subtraction(x,y){
+    let result = x - y;
+    document.getElementById('Subtraction').innerHTML = "Subtraction of 100-21 =" +result;
+}
+
+function multiplication(x,y){
+    let result = x * y;
+    document.getElementById('Multiplication').innerHTML = "Multiplication of 100*21 =" +result;
+}
+
+function exponentiation(x,y){
+    let result = x ** y;
+    document.getElementById('Exponentiation').innerHTML = "Exponentiation of 100**2 =" +result;
+}
+
+function division(x,y){
+    let result = x / y;
+    document.getElementById('Division').innerHTML = "Division of 100/21 =" +result;
+}
+
+function modulus(x,y){
+    let result = x % y;
+    document.getElementById('modulus').innerHTML = "Modulus of 100%21 =" +result;
+}
+
+
+function increment(x){
+    x++;
+    let result = x;
+    document.getElementById('increment').innerHTML = "increment value of 101++:  =" +result;
+}
+
+function decrement(x){
+    x--;
+    let result = x;
+    document.getElementById('decrement').innerHTML = "decrement value of 101--: =" +result;
+}
+
+
+
+// This code of Getformdata.html
+
+function addition(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1+num2;
+}
+
+function subtraction(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1-num2;
+}
+
+function multiplication(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1*num2;
+}
+
+function exponentiation(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1**num2;
+}
+
+function division(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1/num2;
+}
+
+function modulus(){
+  let value1 = document.getElementById('firstNumber').value;
+  let value2 = document.getElementById('secondNumber').value;
+  num1 =  Number(value1);
+  num2 =  Number(value2)
+  
+  document.getElementById('Output').innerHTML = num1%num2;
+}
